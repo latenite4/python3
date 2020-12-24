8b82e6330766007eef398bbda8f1fefe677a50f4
latenite4 <rnm@pobox.com>
2020-12-24 13:10:15 -0600

    adding madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,17 @@
+
+
+#do string concat
+youtuber=""
+
+print(" subscribe to "+youtuber)
+print("subscribe to {}".format(youtuber))
+print(f"subscribe to {youtuber}")
+
+adj = input("adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person=input("famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited \
+because i love to {verb1}. Stay hydrated and {verb2} like you are a {famous_person}!"
+print(madlib)
