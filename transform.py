651d5bd5a0f0c6e4376629693f92c77231c21f2a
latenite4 <rnm@pobox.com>
2021-01-03 10:45:36 -0600

    added argparse for various CLI args/params

--- transform.py
+++ transform.py
@@ -1,7 +1,22 @@
 
+#!/usr/bin/python3
+#program to parse png images and change images
+# cmd: python3 transform.py
+# you must have local input/ and output/ directories
+#
+# name: R. Melton
+# date: 12/27/20
+# cmdline: python transform.py cmd show image='city.png' --ulx=1 --uly=2 --brx=0 --bry=9
+#          python transform.py show city.png
+#          python transform.py blur city.png
+
 from image import Image
 import numpy as np
-import time, os
+import time, os, argparse, string
+#from tkinter import *
+import imghdr
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
 
 def adjust_brightness(image,factor):
   #scale each value by some amount
@@ -31,11 +46,15 @@ def adjust_contrast(image, factor, mid=0.5):
   # new_im.array = (image.array - mid) * factor + mid
 
   return new_im
+
+# blur and image
 def blur(image, k_size):
   #k_size is the number of pixels to use when doing the blur
   #k_size=3 would be above and below and left neighbor, right neighbor pixels, and diagonal
   #neighbor pixels.
-  x_pixels, y_pixels,num_channels = image.array.shape
+  im = Image(filename = image)
+
+  x_pixels, y_pixels,num_channels = im.array.shape
   new_im = Image(x_pixels=x_pixels,y_pixels=y_pixels,num_channels=num_channels)
   neighbor_range = k_size // 2
  
@@ -86,7 +105,14 @@ def combine_images(image1, image2):
     return new_im
 
 
-def check_env():
+def show_image(in_image):
+  path="input/"
+  img = mpimg.imread(path+in_image)
+  imgplot = plt.imshow(img)
+  plt.show()
+
+# check for necessary parts of the run time environment
+def check_env( in_image):
   #check to verify that output/input dirs exist:
   path = './output/'
   is_path = os.path.isdir(path)  
@@ -108,10 +134,58 @@ def check_env():
     print(quit)
     quit()
 
+  #verify input image
+  if in_image:
+    thefile = 'input/'+in_image
+    print('file path: '+thefile)
+    is_file = os.path.isfile(thefile) 
+    if not is_file:
+      print(f'local ./input file {in_image} must exist, cannot continue...')
+      print(quit)
+      quit()
 
+  if imghdr.what(thefile) != 'png':
+    print('wrong image file type, cannot continue...')
+    print(quit)
+    quit()
+    
+
+def cmd():
+  print("routine cmd")
+
+# setup command line args and parms
+#   optional args have --
+#   fixed (required args do not have --)
+def arg_init():
+  parser = argparse.ArgumentParser(description='Process an image.')
+  parser.add_argument("cmd",help="command to this program",type=str)
+  parser.add_argument("image",help="input image name for the command",type=str)
+  
+  parser.add_argument("--ulx",action='store_true',help="upperleft x in image")
+  parser.add_argument("--uly",action='store_true',help="upperleft y in image")
+  parser.add_argument("--brx",action='store_true',help="bottomright x in image")
+  parser.add_argument("--bry",action='store_true',help="bottomright y in image")
+
+  group = parser.add_mutually_exclusive_group()
+  group.add_argument('--v', action='store_true',help="add more text output")
+  group.add_argument('--q', action='store_true',help="minimal output")
+
+  args = parser.parse_args()
+  print(args.image)
+
+
+  #if args.cmd != "show" and args.cmd != "blur":
+
+
+  return args
+
+#def show_image(filename):
+
+ 
 if __name__ == '__main__':
 
-  check_env()
+  args = arg_init()
+  check_env(args.image)
 
   lake = Image(filename = 'lake.png')
   city = Image(filename='city.png')
@@ -134,13 +208,22 @@ if __name__ == '__main__':
 
 
 # let's apply a sobel kernel on the x and y axis
-  sobel_x = apply_kernel(city, np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]]))
-  sobel_x.write_image('edge_x.png')
-  sobel_y = apply_kernel(city, np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]]))
-  sobel_y.write_image('edge_y.png')
+  # sobel_x = apply_kernel(city, np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]]))
+  # sobel_x.write_image('edge_x.png')
+  # sobel_y = apply_kernel(city, np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]]))
+  # sobel_y.write_image('edge_y.png')
 
-  # this will show x and y edges
-  sobel_xy = combine_images(sobel_x, sobel_y)
-  sobel_xy.write_image('edge_xy.png')
+  # # this will show x and y edges
+  # sobel_xy = combine_images(sobel_x, sobel_y)
+  # sobel_xy.write_image('edge_xy.png')
 
-  print(f'total execution duration: {time.time() - start_time}s')
+  if args.cmd == "show" and args.image:
+    show_image(args.image)
+  if args.cmd == "blur" and args.image:
+    blur_15 = blur(args.image,15)
+    blur_15.write_image(args.image+'blur_k15.png') 
+    show_image(blur_k15.png)
+    
+  if args.v:
+    print(f'total execution duration: {time.time() - start_time}s')
+    
